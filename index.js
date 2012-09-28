10e52fa9eaae672aebfc72a2e3e65473c6d725f8
libra guest <magalhas@gmail.com>
2012-09-28 17:01:16 -0400

    version 0.2.2 changed the way the module is exported, now it can be instanced instead of being a singleton

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var commander = require('commander'),
 
 if (!module.parent) {
     commander
-        .version('0.2.1')
+        .version('0.2.2')
         .option('-c, --config [path]', 'Path to the configuration file')
         .option('-p, --port [port]', 'Port that the http mock server will use')
         .option('-r, --rootpath [path]', 'http mock server root path for static files')
         .parse(process.argv);
-        
+
     if (commander.port) {
         nconf.overrides({'serverPort': commander.port});
     }
-    
+
     if (commander.rootpath) {
         nconf.overrides({'serverRootPath': commander.rootpath});
     }
@@ -25,5 +25,5 @@ if (!module.parent) {
         .createWebServices()
         .start();
 } else {
-    exports = module.exports = new HttpdMock();
+    exports = module.exports = HttpdMock;
 }
